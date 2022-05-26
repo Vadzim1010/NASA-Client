7d9828e953d5fe9d3e14cc2998fb31a1e7fcc785
Vadzim1010 <82838234+Vadzim1010@users.noreply.github.com>
2022-05-26 14:39:40 +0300

    adding image of day screen

--- app/src/main/java/com/example/nasa/koin/ViewModelModule.kt
+++ app/src/main/java/com/example/nasa/koin/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.example.nasa.koin
 
 import com.example.nasa.repository.impl.RemoteRepositoryImpl
+import com.example.nasa.ui.day.ImageOfDayViewModel
 import com.example.nasa.ui.description.DescriptionViewModel
 import com.example.nasa.ui.image.NasaImageViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -10,7 +11,12 @@ val viewModelModule = module {
     viewModel {
         NasaImageViewModel(get())
     }
+
     viewModel {
         DescriptionViewModel(get<RemoteRepositoryImpl>())
+    }
+
+    viewModel {
+        ImageOfDayViewModel()
     }
 }
