bd744b28390d086f4924b709e6f39a0051e02067
Vadzim1010 <82838234+Vadzim1010@users.noreply.github.com>
2022-06-23 14:27:05 +0300

    add search button

--- app/src/main/java/com/example/nasa/adapter/holder/NasaImageViewHolder.kt
+++ app/src/main/java/com/example/nasa/adapter/holder/NasaImageViewHolder.kt
@@ -4,6 +4,8 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.size.Scale
 import coil.size.ViewSizeResolver
+import coil.transform.CircleCropTransformation
+import com.example.nasa.R
 import com.example.nasa.databinding.ItemNasaImageBinding
 import com.example.nasa.domain.model.NasaImage
 
@@ -15,8 +17,8 @@ class NasaImageViewHolder(
     fun bind(image: NasaImage) {
         with(binding) {
             nasaImage.load(image.imageUrl) {
-                scale(Scale.FILL)
-                size(ViewSizeResolver(root))
+                crossfade(true)
+                crossfade(500)
             }
             card.setOnClickListener {
                 onClickListener(image.id)
